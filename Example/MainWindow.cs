ed71af249030624812904cc32790715aa1ed84be
Simple <78424351+Simple-2021@users.noreply.github.com>
2023-02-26 15:53:54 +0800

    Update csproj

--- Example/MainWindow.cs
+++ Example/MainWindow.cs
@@ -1,4 +1,5 @@
 using LibMaterial;
+using System.Diagnostics;
 using static LibMaterial.LibImport.DwmSystemBackdropTypeFlgs;
 
 namespace Example
@@ -9,10 +10,11 @@ namespace Example
         {
             InitializeComponent();
             Chk1.Checked =
-            Chk2.Checked =
+            Chk2.Checked = true;
             Chk3.Checked =
             SBT_2.Checked = true;
         }
+
         private void MainWindow_SystemColorsChanged(object sender, EventArgs e)
         {
             Chk2.Checked = LibRegistry.GetAppUseLightTheme();
@@ -25,17 +27,35 @@ namespace Example
             {
                 case 1:
                     {
-                        LibApply.Apply_Backdrop_Effect(Handle, !ApplyBox.Checked, BackdropFlag);
+                        if (ApplyBox.Checked)
+                        {
+                            LibApply.Apply_Backdrop_Effect(Handle, BackdropFlag);
+                        }
+                        else
+                        {
+                            LibApply.Cancel_Backdrop_Effect(Handle);
+                        }
                     }
                     break;
                 case 2:
                     {
                         LibApply.Apply_Light_Theme(Handle, !ApplyBox.Checked);
+                        if (Chk3.Checked)
+                        {
+                            LibApply.Apply_Transparent_Form(Handle, !Chk2.Checked);
+                        }
                     }
                     break;
                 case 3:
                     {
-                        LibApply.Apply_Transparent_Form(Handle, !ApplyBox.Checked);
+                        if (ApplyBox.Checked)
+                        {
+                            LibApply.Apply_Transparent_Form(Handle, !Chk2.Checked);
+                        }
+                        else
+                        {
+                            LibApply.Cancel_Transparent_Form(Handle);
+                        }
                     }
                     break;
             }
@@ -74,7 +94,7 @@ namespace Example
                     }
                     break;
             }
-            LibApply.Apply_Backdrop_Effect(Handle, !Chk1.Checked, BackdropFlag);
+            LibApply.Apply_Backdrop_Effect(Handle, BackdropFlag);
             DescriptionBox.Text = LibApply.About_Enum_Description(BackdropFlag);
         }
 
--- LibMaterial/LibApply.cs
+++ LibMaterial/LibApply.cs
@@ -9,23 +9,40 @@ namespace LibMaterial
 {
     public static class LibApply
     {
+        [Description("亮色")]
+        public static Color LightColor = Color.FromArgb(233, 233, 234);
+        [Description("暗色")]
+        public static Color DarkColor = Color.FromArgb(22, 22, 21);
+
         [Description("在 Win32 应用中支持背景材质")]
-        public static void Apply_Backdrop_Effect(IntPtr HWnd, bool Disable = false, DwmSystemBackdropTypeFlgs BackdropFlag = DWMSBT_MAINWINDOW)
+        public static void Apply_Backdrop_Effect(IntPtr HWnd, DwmSystemBackdropTypeFlgs BackdropFlag = DWMSBT_MAINWINDOW)
         {
-            int Key = (int)(Disable ? DWMSBT_NONE : BackdropFlag);
-            DwmSetWindowAttribute(HWnd, DWM_SYSTEMBACKDROP_TYPE, ref Key, Marshal.SizeOf(typeof(int)));
+            int key = (int)BackdropFlag;
+            DwmSetWindowAttribute(HWnd, DWM_SYSTEMBACKDROP_TYPE, ref key, Marshal.SizeOf(typeof(int)));
         }
         [Description("在 Win32 应用中支持深色和浅色主题")]
-        public static void Apply_Light_Theme(IntPtr HWnd, bool Disable = false)
+        public static void Apply_Light_Theme(IntPtr HWnd, bool Dark = false)
         {
-            int Key = Disable ? 1 : 0;
-            DwmSetWindowAttribute(HWnd, DWM_USE_IMMERSIVE_DARK_MODE, ref Key, Marshal.SizeOf(typeof(int)));
+            int key = Dark ? 1 : 0;
+            DwmSetWindowAttribute(HWnd, DWM_USE_IMMERSIVE_DARK_MODE, ref key, Marshal.SizeOf(typeof(int)));
         }
         [Description("在 Win32 应用中支持透明客户端区域")]
-        public static void Apply_Transparent_Form(IntPtr HWnd, bool Disable = false)
+        public static void Apply_Transparent_Form(IntPtr HWnd, bool Dark = false)
         {
             Form form = (Form)Control.FromHandle(HWnd);
-            form.TransparencyKey = Disable ? Color.Empty : form.BackColor = CalcTransparentColor(form.BackColor);
+            form.ForeColor = Dark ? LightColor : DarkColor;
+            form.TransparencyKey = form.BackColor = Dark ? DarkColor : LightColor;
+        }
+        [Description("在 Win32 应用中取消背景材质")]
+        public static void Cancel_Backdrop_Effect(IntPtr HWnd)
+        {
+            int Key = 0;
+            DwmSetWindowAttribute(HWnd, DWM_SYSTEMBACKDROP_TYPE, ref Key, Marshal.SizeOf(typeof(int)));
+        }
+        [Description("在 Win32 应用中取消透明客户端区域")]
+        public static void Cancel_Transparent_Form(IntPtr HWnd)
+        {
+            ((Form)Control.FromHandle(HWnd)).TransparencyKey = Color.Empty;
         }
         [Description("获取关于指定方法的描述")]
         public static string? About_Method_Description(string MethodName)
